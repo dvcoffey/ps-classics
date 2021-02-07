39df218ca4a4aa34286e915f976ac87c02b81134
dvcoffey <dvcoffey@gmail.com>
2021-02-07 13:48:18 +0000

    updated task input fields

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
+    $('.datepicker').datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 50,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
 });
