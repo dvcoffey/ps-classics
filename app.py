463a72a73b433d3c5cbb2bf1f37c66601aaccbad
dvcoffey <dvcoffey@gmail.com>
2021-02-13 08:50:35 +0000

    add comments to app.py code

--- app.py
+++ app.py
@@ -21,6 +21,12 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/index")
 def index():
+    """
+    List items by most recently added to database
+    List items by most page views
+    List genres to browse by genre
+    Return index.html
+    """
     recents = list(mongo.db.games.find().sort("time_stamp", -1).limit(6))
     views = list(mongo.db.games.find().sort("page_views", -1).limit(6))
     genres = list(mongo.db.genres.find().sort("genre_name", 1))
@@ -30,6 +36,11 @@ def index():
 
 @app.route("/games")
 def games():
+    """
+    Find games with search query if field has value
+    Else find all games
+    Return games.html
+    """
     query = request.args.get("query")
     if query:
         games = mongo.db.games.find(({"$text": {"$search": query}}))
@@ -38,24 +49,33 @@ def games():
     return render_template("games.html", games=games)
 
 
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    '''
+    Get query by form input
+    Find games by matching query
+    Return games.html
+    '''
+    query = request.form.get("query")
+    games = mongo.db.games.find(({"$text": {"$search": query}}))
+    return render_template("games.html", games=games)
+
+
 @app.route("/details/<game_id>")
 def details(game_id):
-    '''Open game details'''
+    '''
+    Find game by game_id
+    Return game-details.html
+    '''
     game = mongo.db.games.find_one({"_id": ObjectId(game_id)})
     mongo.db.games.update(
         {"_id": ObjectId(game_id)}, {'$inc': {'page_views': +1}})
     return render_template("game-details.html", game=game)
 
 
-@app.route("/search", methods=["GET", "POST"])
-def search():
-    query = request.form.get("query")
-    games = mongo.db.games.find(({"$text": {"$search": query}}))
-    return render_template("games.html", games=games)
-
-
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    # register new user to database
     if request.method == "POST":
         # check if username already exists in database
         existing_user = mongo.db.users.find_one(
@@ -75,12 +95,17 @@ def register():
         session["user"] = request.form.get("username").lower()
         flash("Registration Succesful!")
         return redirect(url_for("profile", username=session["user"]))
+
+    # If user already exists
     return render_template("register.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+
+    # log in existing user
     if request.method == "POST":
+
         # check if username exists in db
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
@@ -113,6 +138,10 @@ def profile(username):
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
 
+    '''
+    list games added to database by user
+    return profile.html
+    '''
     if session["user"]:
         games = list(mongo.db.games.find(
             {"added_by": session["user"]}).sort("time_stamp", -1))
@@ -131,7 +160,13 @@ def logout():
 
 @app.route("/add_game", methods=["GET", "POST"])
 def add_game():
+    '''
+    request input fields from form data
+    add new game to the database
+    return games.html
+    '''
     if request.method == "POST":
+        # time_stamp stores datetime to a string to by used by database
         time_stamp = str(datetime.now())
         game = {
             "game_name": request.form.get("game_name"),
@@ -148,12 +183,18 @@ def add_game():
         flash("Game Added To Database")
         return redirect(url_for("games"))
 
+    # retrieves genres for dropdown selector
     genres = mongo.db.genres.find().sort("genre_name", 1)
     return render_template("add-game.html", genres=genres)
 
 
 @app.route("/edit_game/<game_id>", methods=["GET", "POST"])
 def edit_game(game_id):
+    '''
+    request input fields from form data
+    update an existing game in the database
+    return games.html
+    '''
     if request.method == "POST":
         edit = {
             "game_name": request.form.get("game_name"),
@@ -168,7 +209,9 @@ def edit_game(game_id):
         mongo.db.games.update({"_id": ObjectId(game_id)}, edit)
         flash("Game Edited")
 
+    # find game by ObjectId
     game = mongo.db.games.find_one({"_id": ObjectId(game_id)})
+    # retrieves genres for dropdown selector
     genres = mongo.db.genres.find().sort("genre_name", 1)
     return render_template("edit-game.html", game=game, genres=genres)
 
@@ -182,12 +225,21 @@ def delete_game(game_id):
 
 @app.route("/genres")
 def genres():
+    '''
+    retrieve list of genres in database
+    return genres.html
+    '''
     genres = list(mongo.db.genres.find().sort("genre_name", 1))
     return render_template("genres.html", genres=genres)
 
 
 @app.route("/add_genre", methods=["GET", "POST"])
 def add_genre():
+    '''
+    request input fields from form data
+    add new genre to the database
+    return add-genre.html
+    '''
     if request.method == "POST":
         genre = {
             "genre_name": request.form.get("genre_name")
@@ -196,11 +248,17 @@ def add_genre():
         flash("Genre Added Succesfully")
         return redirect(url_for("genres"))
 
+    # if form input is cancelled
     return render_template("add-genre.html")
 
 
 @app.route("/edit_genre/<genre_id>", methods=["GET", "POST"])
 def edit_genre(genre_id):
+    '''
+    request input fields from form data
+    update an existing genre in the database
+    return genres.html
+    '''
     if request.method == "POST":
         edit = {
             "genre_name": request.form.get("genre_name")
@@ -209,6 +267,7 @@ def edit_genre(genre_id):
         flash("Genre Successfully Updated")
         return redirect(url_for("genres"))
 
+    # if form input is cancelled
     genre = mongo.db.genres.find_one({"_id": ObjectId(genre_id)})
     return render_template("edit-genre.html", genre=genre)
 
@@ -222,6 +281,10 @@ def delete_genre(genre_id):
 
 @app.route("/filter_genre/<genre_name>", methods=["GET", "POST"])
 def filter_genre(genre_name):
+    '''
+    retrieve games from database that contain a specific genre
+    return games.html with filter applied
+    '''
     games = list(mongo.db.games.find({"genre_name": genre_name}))
     return render_template("games.html", games=games)
 
