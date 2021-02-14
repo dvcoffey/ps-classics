6111b417c7d30aceef72401e0517960151e92595
dvcoffey <dvcoffey@gmail.com>
2021-02-14 15:40:14 +0000

    505.html added and connected to app.py

--- app.py
+++ app.py
@@ -276,11 +276,14 @@ def filter_genre(genre_name):
 
 
 
-# 404 page function
 @app.errorhandler(404)
 def page_not_found(error):
-    # the 404 status is set explicitly
     return render_template('404.html', title='404'), 404
+
+
+@app.errorhandler(500)
+def internal_server(error):
+    return render_template('500.html', title='500'), 500
 
 
 if __name__ == "__main__":
