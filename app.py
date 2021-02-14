31561a92d19790327d13a701ecb6949ffd89c286
dvcoffey <dvcoffey@gmail.com>
2021-02-14 16:21:07 +0000

    code checked for indents and spacing, further comments added, 505.html added

--- app.py
+++ app.py
@@ -275,12 +275,13 @@ def filter_genre(genre_name):
     return render_template("games.html", games=games)
 
 
-
+# 404 Error Handler
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html', title='404'), 404
 
 
+# 505 Error Handler
 @app.errorhandler(500)
 def internal_server(error):
     return render_template('500.html', title='500'), 500
